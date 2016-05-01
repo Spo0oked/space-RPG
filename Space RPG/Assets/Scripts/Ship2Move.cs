3e04de3c25341a4c5b515d8a20f1a4787184a013
Spo0oked <sagan.richardson@me.com>
2016-04-30 20:40:24 -0700

    added proj

---
+++ Space RPG/Assets/Scripts/Ship2Move.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ship2Move : MonoBehaviour {
+
+	public Rigidbody2D rb; //instance variable of 2d rigid body
+	public float speed;
+
+	public float speedFector;
+	Vector3 mousePositionInWorld;
+	float angle;
+	public float startRotationOffset = 90;  //This is angle offset at starting.
+
+	private Animator anim;
+
+	bool pressed;
+
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D> (); //returning the rigid body
+		anim = GetComponent<Animator>(); //returning animator
+	}
+	
+
+	void FixedUpdate () {
+		
+		float Vinput = Input.GetAxis ("Vertical");
+		float boost = speed * 2;
+		rb.AddForce (gameObject.transform.up * speed * Vinput); //adding mocement for up/down
+
+		if (Input.GetKey (KeyCode.LeftShift)) {
+			rb.AddForce (gameObject.transform.up * boost * Vinput); //raising spped for a sprint function
+		}
+	
+		mousePositionInWorld = Camera.main.ScreenToWorldPoint (Input.mousePosition); //geting mouse pos
+		angle = ( Mathf.Atan2 (mousePositionInWorld.y - transform.position.y, mousePositionInWorld.x - transform.position.x) * Mathf.Rad2Deg); //finding angle to mouse
+
+		transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.Euler(0, 0, angle+startRotationOffset),speedFector * Time.deltaTime); //rotating ship
+
+		if (!pressed && Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.UpArrow)) { //checking if w or up is pressed and shift is not
+			anim.SetBool ("reg", true);
+			anim.SetBool ("idle", false);
+			anim.SetBool ("boost", false);
+		} else if (!Input.GetKey (KeyCode.W) || !Input.GetKey (KeyCode.UpArrow) && !pressed) {
+			anim.SetBool ("reg", false);
+			anim.SetBool ("idle", true);
+			anim.SetBool ("boost", false);
+		}
+	}
+}
---
+++ Space RPG/Assets/Scripts/camera.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera : MonoBehaviour {
+
+
+
+	public float interpVelocity;
+	public float minDistance;
+	public float followDistance;
+	public float follow;
+	public GameObject target;
+	public Vector3 offset;
+	Vector3 targetPos;
+	// Use this for initialization
+	void Start () {
+		targetPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		if (target)
+		{
+			Vector3 posNoZ = transform.position;
+			posNoZ.z = target.transform.position.z;
+			
+			Vector3 targetDirection = (target.transform.position - posNoZ);
+			
+			interpVelocity = targetDirection.magnitude * follow;
+
+			targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime); 
+			
+			transform.position = Vector3.Lerp( transform.position, targetPos + offset, 0.25f);
+		
+
+
+		}
+
+	}
+	void Update () {
+		//transform.position.x = Input.mousePosition.x;
+		//transform.position.z = Input.mousePosition.y; 
+		// error CS1612: Cannot modify a value type return value of `UnityEngine.Transform.position'. 
+		// Consider storing the value in a temporary variable
+		
+		//Vector3 thePos = new Vector3( Input.mousePosition.x, transform.position.y, Input.mousePosition.y ); // temporary variable
+		//transform.position = thePos;
+	} 
+	}
+
